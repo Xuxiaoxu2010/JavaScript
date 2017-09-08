c311a72d2fd9022471df3c88ba52eda6a21447e1
He Wei <cnhwvista@gmail.com>
2017-09-08 21:59:55 +0800

    update

--- public/lesson7/process.js
+++ public/lesson7/process.js
@@ -1,73 +1,80 @@
-'use strict';
+// use strict 的合理用法
+// https://stackoverflow.com/a/4462560/2667665
+(function () {
+    'use strict';
 
-var fs = require('fs'),
-    path = require('path'),
-    jsonfile = require('jsonfile');
+    /* 引入 simple-git 这个库
+       每次输出心里话之前，先自动更新一下，保证内容是最新的 */
 
-var repoPath = path.join(__dirname, '../../../words'),
-    contentPath = path.join(__dirname, 'content.json'),
-    errorPath = path.join(__dirname, 'error.json');
+    var fs = require('fs'),
+        path = require('path'),
+        jsonfile = require('jsonfile');
 
-var jsonFiles = [],
-    errorFiles = [],
-    fileContent = [];
+    var repoPath = path.join(__dirname, '../../../words'),
+        contentPath = path.join(__dirname, 'content.json'),
+        errorPath = path.join(__dirname, 'error.json');
 
-// https://stackoverflow.com/a/4482701/2667665
-if (!fs.existsSync(repoPath)) {
+    var jsonFiles = [],
+        errorFiles = [],
+        fileContent = [];
 
-    console.log('心里话项目的路径不正确，请检查路径');
-    return -1;
+    // https://stackoverflow.com/a/4482701/2667665
+    if (!fs.existsSync(repoPath)) {
 
-}
-
-var files = fs.readdir(repoPath, function (err, files) {
-
-    if (err) {
-        console.log('读取文件夹失败，请检查原因');
+        console.log('心里话项目的路径不正确，请检查路径');
         return -1;
+
     }
 
-    for (var i = files.length - 1; i >= 0; i--) {
+    var files = fs.readdir(repoPath, function (err, files) {
 
-        var fullPath = path.join(repoPath, files[i]);
-
-        if (fs.statSync(fullPath).isDirectory()) {
-
-            files.splice(i, 1);
-
-        } else if (!files[i].endsWith('.json') &&
-            fs.statSync(fullPath).isFile()) {
-
-            files.splice(i, 1);
-        } else {
-
-            // 提前就把文件的路径拼接好
-            files[i] = fullPath;
+        if (err) {
+            console.log('读取文件夹失败，请检查原因');
+            return -1;
         }
-    }
 
-    jsonFiles = files;
+        // jshint: i is already defined
+        // when defined in both for loops
+        // https://stackoverflow.com/questions/19412727/
+        var i;
 
-    for (var i = 0; i < jsonFiles.length; i++) {
+        for (i = files.length - 1; i >= 0; i--) {
 
-        try {
+            var fullPath = path.join(repoPath, files[i]);
 
-            // 如果用异步读取，读取失败的文件名无法传入回调函数
-            var content = jsonfile.readFileSync(jsonFiles[i]);
+            if (fs.statSync(fullPath).isDirectory()) {
 
-            fileContent.push(content);
+                files.splice(i, 1);
 
-        } catch (ex) {
+            } else if (!files[i].endsWith('.json') &&
+                fs.statSync(fullPath).isFile()) {
 
-            // console.log(ex.message);
-            errorFiles.push(path.basename(jsonFiles[i]));
+                files.splice(i, 1);
+            } else {
+
+                // 提前就把文件的路径拼接好
+                files[i] = fullPath;
+            }
         }
-    }
 
-    // for (var i = 0; i < fileContent.length; i++) {
-    //     fileContent[i] = JSON.stringify(fileContent[i], null, 4);
-    // }
+        jsonFiles = files;
 
-    jsonfile.writeFileSync(contentPath, fileContent);
-    jsonfile.writeFileSync(errorPath, errorFiles);
-});
+        for (i = 0; i < jsonFiles.length; i++) {
+
+            try {
+
+                // 如果用异步读取，读取失败的文件名无法传入回调函数
+                var content = jsonfile.readFileSync(jsonFiles[i]);
+
+                fileContent.push(content);
+
+            } catch (ex) {
+
+                errorFiles.push(path.basename(jsonFiles[i]));
+            }
+        }
+
+        jsonfile.writeFileSync(contentPath, fileContent);
+        jsonfile.writeFileSync(errorPath, errorFiles);
+    });
+})();
