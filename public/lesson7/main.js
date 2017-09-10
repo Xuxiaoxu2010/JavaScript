14708ec17c5c7690815a237815d993192f9b2aaf
He Wei <cnhwvista@gmail.com>
2017-09-10 15:37:01 +0800

    update

--- public/lesson7/main.js
+++ public/lesson7/main.js
@@ -7,6 +7,7 @@
     $.ajax({
         url: "https://raw.githubusercontent.com/Dream4ever/JavaScript/master/public/lesson7/content.json",
         success: function (data, status) {
+            
             result = JSON.parse(data);
 
             var vm = new Vue({
