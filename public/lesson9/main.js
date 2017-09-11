db14bfb9eeee1812196391c2493463a27e255729
He Wei <cnhwvista@gmail.com>
2017-09-11 22:09:33 +0800

    update

--- public/lesson9/main.js
+++ public/lesson9/main.js
@@ -1,8 +1,9 @@
+// 让jshint支持es6，不再提示箭头函数的事情
 // https://stackoverflow.com/a/42870745/2667665
 /*jshint esversion: 6 */
 
-
 // 2017年09月11日更新：今天调用axios又没问题了……这是什么fuck？
+
 // 如果将js单独写在一个文件中，vue调用axios会失败，所以需要用import引入
 // 而import是ES6的语法，目前的浏览器并不支持，又需要用Babel将JS转码才行
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
@@ -22,6 +23,8 @@
 // import - MDN
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
 
+// SOF上一篇非常到位的解答，说明了在JS文件中为什么要用IIFE将整个文件里的代码全部封装起来
+// https://stackoverflow.com/a/2421949/2667665
 (function () {
 
     'use strict';
@@ -31,6 +34,7 @@
     // words: 心里话       detail: 课程基本信息      catalog: 课程文档信息
     // homework/1 课程作业  teams: 团队信息          sayToMe: 给老师留言
 
+    // 箭头函数中如果有大括号，就必须有return，如果没有大括号，比如下面的形式，可以不return
     // 正确写法
     // var apiList =
     //     ['course/1/words',
@@ -54,7 +58,7 @@
     //             api = preFix + api;
     //         });
 
-    // API列表不采用数组形式而是采用对象形式，因为这样使用变量时一目了然
+    // API列表不采用数组形式而是采用对象形式，这样使用变量的时候从字面上就能够看出来调用的是什么接口
     var api = {
         'words': 'course/1/detail',
         'catalog': 'course/1/catalog',
@@ -78,7 +82,7 @@
                 // https://stackoverflow.com/a/13192501/2667665
                 axios.get(api.words)
                     .then(function (response) {
-                        console.log(response);
+                        console.log('data fetched:\n' + response);
                         app.wordsList = response.data.words;
                     })
                     .catch(function (error) {
@@ -91,16 +95,17 @@
         }
     });
 
-    vm.fetchData();
+    // vm.fetchData();
 
     var body = {
         'name': '何伟',
         'account': '15564866632',
-        'content': '经过这九节课的学习，从最初研究 git 和 GitHub 的使用，到对编程中一些关键概念的思考、理解，包括回调函数、异步函数，一边看文章、看代码琢磨这些概念，一边自己动手写代码，并解决遇到的各种问题，在这个过程中，真正收获了成长。不同于以往的教科书式的学习，这次的课程，通过让大家以听课+自主学习的方式，去走完JS入门学习过程中的几个关键路径，这样能够对JS这门编程语言的方方面面，以及技术团队的实际工作流程及方法有自己的实操经验及领悟。虽然自己在编程方面已经不算是小白了，但是经过这几个星期的课程的学习，所带来的收获是受用终生的。一方面让自己逐渐开始养成了写技术文档的习惯，另外也让自己更加勇于尝试新技术，感觉在课程的学习过程中，渐渐地摸到了"学习的方法"，这种感觉是很美妙的。最后，强烈期待徐老师的进阶课程，这几个星期的学习让自己已经沉迷在JS之中了，真的是停不下来~~~'
+        'content': '经过这九节课的学习，从最初研究 git 和 GitHub 的使用，到对编程中一些关键概念的思考、理解，包括回调函数、异步函数，一边看文章、看代码琢磨这些概念，一边自己动手写代码，并解决遇到的各种问题，在这个过程中，真正收获了成长。不同于以往的教科书式的学习，这次的课程，通过让大家以听课+自主学习的方式，去走完JS入门学习过程中的几个关键路径，这样能够对JS这门编程语言的方方面面，以及技术团队的实际工作流程及方法有自己的实操经验及领悟。虽然自己在编程方面已经不算是小白了，但是经过这几个星期的课程的学习，所带来的收获是受用终生的。一方面让自己逐渐开始养成了写技术文档的习惯，另外也让自己更加勇于尝试新技术，感觉在课程的学习过程中，渐渐地摸到了"学习的方法"，这种感觉是很美妙的。最后，强烈期待徐老师的进阶课程，这几个星期的学习让自己已经沉迷在JS之中了，真的是停不下来~~~\n再测试一下发图功能~~~\n</div><div><img src="https://raw.githubusercontent.com/Dream4ever/Pics/master/javascript-code.jpeg"></div>'
     };
 
     axios.post('https://js.xinshengdaxue.com/api/v1/learnJS/sayToMe', body)
         .then(function (response) {
+            console.log('data posted:\n');
             console.log(response);
         })
         .catch(function (error) {
