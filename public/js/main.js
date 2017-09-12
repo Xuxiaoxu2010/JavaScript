b4ab11ba8c44bb03d5c02e997c2448e8c89c70c1
He Wei <cnhwvista@gmail.com>
2017-09-12 14:28:38 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -29,10 +29,26 @@
 
     'use strict';
 
-    var preFix = 'https://js.xinshengdaxue.com/api/v1/learnJS/';
-
     // words: 心里话       detail: 课程基本信息      catalog: 课程文档信息
     // homework/1 课程作业  teams: 团队信息          sayToMe: 给老师留言
+
+    // API列表不采用数组形式而是采用对象形式，这样使用变量的时候从字面上就能够看出来调用的是什么接口
+    var api = {
+        'words': 'course/1/words',
+        'detail': 'course/1/detail',
+        'catalog': 'course/1/catalog',
+        'homework': 'course/1/homework',
+        'teams': 'course/1/teams',
+        'sayToMe': 'sayToMe'
+    };
+
+    var preFix = 'https://js.xinshengdaxue.com/api/v1/learnJS/';
+
+    // jshint提示不要使用for in？不过尝试一下别的遍历对象属性的方法也是蛮不错的
+    // https://stackoverflow.com/a/14379351/2667665
+    Object.keys(api).forEach(function (key) {
+        api[key] = preFix + api[key];
+    });
 
     // 箭头函数中如果有大括号，就必须有return，如果没有大括号，比如下面的形式，可以不return
     // 正确写法
@@ -58,28 +74,10 @@
     //             api = preFix + api;
     //         });
 
-    // API列表不采用数组形式而是采用对象形式，这样使用变量的时候从字面上就能够看出来调用的是什么接口
-    var api = {
-        'words': 'course/1/detail',
-        'catalog': 'course/1/catalog',
-        'homework': 'course/1/homework',
-        'teams': 'course/1/teams',
-        'sayToMe': 'sayToMe'
-    };
-
-    for (var a in api) {
-        api[a] = preFix + api[a];
-    }
-
-    var body = {
+    var myWord = {
         'name': '何伟',
         'account': '15564866632',
         'content': '经过这九节课的学习，从最初研究 git 和 GitHub 的使用，到对编程中一些关键概念的思考、理解，包括回调函数、异步函数，一边看文章、看代码琢磨这些概念，一边自己动手写代码，并解决遇到的各种问题，在这个过程中，真正收获了成长。不同于以往的教科书式的学习，这次的课程，通过让大家以听课+自主学习的方式，去走完JS入门学习过程中的几个关键路径，这样能够对JS这门编程语言的方方面面，以及技术团队的实际工作流程及方法有自己的实操经验及领悟。虽然自己在编程方面已经不算是小白了，但是经过这几个星期的课程的学习，所带来的收获是受用终生的。一方面让自己逐渐开始养成了写技术文档的习惯，另外也让自己更加勇于尝试新技术，感觉在课程的学习过程中，渐渐地摸到了"学习的方法"，这种感觉是很美妙的。最后，强烈期待徐老师的进阶课程，这几个星期的学习让自己已经沉迷在JS之中了，真的是停不下来~~~'
-    };
-
-    var links = {
-        'github': 'https://github.com/Dream4ever/',
-        'segmentfault': 'https://segmentfault.com/u/samsara0511/articles/'
     };
 
     var vm = new Vue({
@@ -88,44 +86,50 @@
             wordsList: [],
             name: '',
             account: '',
-            content: ''
+            content: '',
+            fetchStatus: false,
+            links: {
+                'home': 'http://js.xinshengdaxue.com',
+                'logo': 'https://o4a7cbihz.qnssl.com/picture/57140c0c-4ffa-4dde-9757-570b53f96796',
+                'avatar': 'https://ws1.sinaimg.cn/large/006tKfTcgy1fi7s7vo8y0j30hs0hsaay.jpg',
+                'github': 'https://github.com/xugy0926/',
+                'mygithub': 'https://github.com/Dream4ever/',
+                'segmentfault': 'https://segmentfault.com/u/samsara0511/articles/'
+            }
         },
         methods: {
-            fetchData: function () {
+            fetch: function () {
 
                 // 尽量使用点运算符访问对象属性
                 // https://stackoverflow.com/a/13192501/2667665
                 axios.get(api.words)
                     .then(function (response) {
-                        console.log('data fetched:\n' + response);
-                        app.wordsList = response.data.words;
+                        if (response.status.toString() === '200' && response.statusText.toString() === 'OK') {
+                            vm.wordsList = response.data.words;
+                            // this.fetchStatus = true;
+                        } else {
+                            // this.fetchStatus = false;
+                        }
                     })
-                    .catch(function (error) {
-                        console.log(error);
+                    .catch(function (err) {
+                        console.log(err);
                     });
             },
             submit: function () {
 
-                // 字符串空值的检测在下面的body赋值语句中无效，放到前面这里才行，为什么？
-                // var user_name = this.name.trim() || body.name,
-                //     user_account = this.account.trim() || body.account,
-                //     user_content = this.content.trim() || body.content;
-
-                console.log(body);
-                console.log(JSON.stringify(body));
-
-                // 为什么必须先stringify再parse才能成功？不能直接parse？
-                // 因为axois接收的data是JSON字符串，而如果直接把对象传过去，
-                // axios就会先按照默认的行为将其转换成字符串，转换结果和stringify是不一样的
-                // 而且不再parse一下的话，就只是单纯的字符串而不是JSON，所以无法得到预期的结果
-                body = JSON.parse(JSON.stringify({
+                // extract();
+                JSON.parse(JSON.stringify({
                     // 为什么必须用这种写法才能检测出空值？
-                    'name': this.name.trim() || body.name,
-                    'account': this.account.trim() || body.account,
-                    'content': this.content.trim() || body.content
+                    'name': this.name.trim() || myWord.name,
+                    'account': this.account.trim() || myWord.account,
+                    'content': this.content.trim() || myWord.content
                 }));
 
-                axios.post(api.sayToMe, body)
+                // TODO: 增加对用户输入的检查，必须全部填写内容后才可发送
+
+                // TODO: 用户发送成功或者失败，都要有反馈
+
+                axios.post(api.sayToMe, myWord)
                     .then(function (response) {
                         console.log('data posted:\n');
                         console.log(response);
@@ -133,9 +137,27 @@
                     .catch(function (error) {
                         console.log(error);
                     });
+            },
+            extract: function () {
+
+                // 字符串空值的检测在下面的myWord赋值语句中无效，放到前面这里才行，为什么？
+                // var user_name = this.name.trim() || myWord.name,
+                //     user_account = this.account.trim() || myWord.account,
+                //     user_content = this.content.trim() || myWord.content;
+
+                // 为什么必须先stringify再parse才能成功？不能直接parse？
+                // 因为axois接收的data是JSON字符串，而如果直接把对象传过去，
+                // axios就会先按照默认的行为将其转换成字符串，转换结果和stringify是不一样的
+                // 而且不再parse一下的话，就只是单纯的字符串而不是JSON，所以无法得到预期的结果
+                return JSON.parse(JSON.stringify({
+                    // 为什么必须用这种写法才能检测出空值？
+                    'name': this.name.trim() || myWord.name,
+                    'account': this.account.trim() || myWord.account,
+                    'content': this.content.trim() || myWord.content
+                }));
             }
         }
     });
 
-    // vm.fetchData();
+    vm.fetch();
 })();
