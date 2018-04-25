b5c3d20b10071f85d246f5ffcde427bbbdd2312f
He Wei <cnhwvista@gmail.com>
2018-04-25 09:57:27 +0800

    :recycle: chore: add config file

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  themeConfig: {
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        title: '岁月留痕',
+        description: '记录学习 JavaScript 的点点滴滴'
+      }
+    },
+    nav: [
+      { text: '博客', link: '/' },
+      { text: 'GitHub', link: 'http://github.com/Dream4ever' },
+    ],
+  }
+}
