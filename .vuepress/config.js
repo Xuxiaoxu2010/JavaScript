c0544f9ad071e1f24d4f667451d2fbe0b2f786cc
He Wei <cnhwvista@gmail.com>
2018-04-25 10:46:07 +0800

    :construction: feat: add title and desc for html meta info

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,32 @@
 module.exports = {
+  title: '岁月留痕',
+  description: '记录编程之路的点点滴滴',
   themeConfig: {
-    locales: {
-      '/': {
-        lang: 'zh-CN',
-        title: '岁月留痕',
-        description: '记录学习 JavaScript 的点点滴滴'
-      }
-    },
     nav: [
       { text: '博客', link: '/' },
       { text: 'GitHub', link: 'http://github.com/Dream4ever' },
     ],
+    sidebar: [
+      ''
+    ]
   }
+}
+
+function genSidebarConfig (path) {
+  return [
+    {
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'basic-config',
+        'assets',
+        'markdown',
+        'using-vue',
+        'custom-themes',
+        'i18n',
+        'deploy'
+      ]
+    }
+  ]
 }
